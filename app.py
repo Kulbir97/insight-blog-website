e51f17811250ff5d19467f87e8d8017b11241e1f
ProjectTeam1-prog <kaurindian8993@gmail.com>
2024-12-10 20:27:29 -0500

    Initial Flask app setup with basic routes

--- app.py
+++ app.py
@@ -1,21 +1,36 @@
-# app.py
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
+# Flask App Setup
 app = Flask(__name__)
-posts = []
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
 
+# Post Model
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+# Home Route
 @app.route('/')
 def index():
+    posts = Post.query.all()
     return render_template('index.html', posts=posts)
 
+# Create Post Route
 @app.route('/create', methods=['GET', 'POST'])
-def create():
+def create_post():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-        posts.append({'title': title, 'content': content})
-        return redirect('/')
+        new_post = Post(title=title, content=content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('index'))
     return render_template('create.html')
 
-if __name__ == '__main__':
+# Run App
+if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
