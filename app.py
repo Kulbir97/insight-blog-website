9d6b586b3de6bdbe9240d69d96adeb90db71b2a4
ProjectTeam1-prog <kaurindian8993@gmail.com>
2024-12-10 20:22:45 -0500

    Added create post functionality

--- app.py
+++ app.py
@@ -1,30 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+# app.py
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-db = SQLAlchemy(app)
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
+posts = []
 
 @app.route('/')
 def index():
-    posts = Post.query.all()
     return render_template('index.html', posts=posts)
 
 @app.route('/create', methods=['GET', 'POST'])
-def create_post():
+def create():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-        new_post = Post(title=title, content=content)
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect(url_for('index'))
+        posts.append({'title': title, 'content': content})
+        return redirect('/')
     return render_template('create.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
